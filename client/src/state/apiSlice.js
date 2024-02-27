a000dec267546018d1b175faff2278f80d82c0a3
Jessica Wang <xuecen.wang@gmail.com>
2024-02-27 14:53:05 -0500

    Redux Toolkit setup and configuration

---
+++ client/src/state/apiSlice.js
@@ -0,0 +1,18 @@
+// Import the RTK Query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+export const apiSlice = createApi({
+    baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
+    reducerPath: "api",
+    tagTypes: ["Kpis"],
+    endpoints: (build) => ({
+        getKpis: build.query({
+            query: () => "kpi/kpis/",
+            providesTags: ["Kpis"]
+        })
+    })
+})
+
+// Export the auto-generated hook for the `getKpis` query endpoint
+export const { useGetKpisQuery } = apiSlice;
--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from './global';
+import { apiSlice } from "./apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     global: globalReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+})
+setupListeners(store.dispatch);
