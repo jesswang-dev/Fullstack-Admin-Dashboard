0945e377738561ab7b3d56226c267d6bd60c5d0f
Jessica Wang <xuecen.wang@gmail.com>
2024-03-07 23:31:14 -0500

    completed the visualization of the Row3 and the Analutics dashboard

--- client/src/scenes/dashboard/Row3.jsx
+++ client/src/scenes/dashboard/Row3.jsx
@@ -48,11 +48,6 @@ const Row3 = () => {
     },
   ];
 
-  //  const pieData = [
-  //    { name: "Group A", value: 400 },
-  //    { name: "Group B", value: 300 },
-  //  ];
-
   //console.log (kpiData)
   const pieChartData = useMemo(() => {
     if (kpiData) {
@@ -68,8 +63,8 @@ const Row3 = () => {
     }
   }, [kpiData]);
 
-  console.log(pieChartData);
-  const pieColors = ["#cfcdef", "#8884d8"];
+  //console.log(pieChartData);
+  const pieColors = ["#8884d8", "#cfcdef"];
 
   return (
     <>
@@ -108,27 +103,57 @@ const Row3 = () => {
       <AnalyticsBox gridArea={"i"}>
         <BoxHeader title="Expense Breakdown By Category" sideText={"+4%"} />
         <FlexBetween>
-          <PieChart width={110} height={100}>
-            <Pie
-              data={pieChartData}
-              innerRadius={18}
-              outerRadius={38}
-              paddingAngle={2}
-              dataKey="value"
-              stroke="none"
-            >
-              {pieChartData[0].map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={pieColors[index % pieColors.length]}
-                />
-              ))}
-            </Pie>
-          </PieChart>
-          <Typography variant="h6">{pieChartData[0].name}</Typography>
+          {pieChartData
+            ? pieChartData.map((data, i) => {
+                return (
+                  <Box key={`box-${data[0].name}-${i}`}>
+                    <PieChart width={110} height={100}>
+                      <Pie
+                        data={data}
+                        innerRadius={18}
+                        outerRadius={38}
+                        paddingAngle={2}
+                        dataKey="value"
+                        nameKey="name"
+                        stroke="none"
+                      >
+                        {data.map((entry, index) => (
+                          <Cell
+                            key={`cell-${index}`}
+                            fill={pieColors[index % pieColors.length]}
+                          />
+                        ))}
+                      </Pie>
+                    </PieChart>
+                    <Typography variant="h6">{data[0].name}</Typography>
+                  </Box>
+                );
+              })
+            : []}
         </FlexBetween>
       </AnalyticsBox>
-      <AnalyticsBox gridArea={"j"}></AnalyticsBox>
+      <AnalyticsBox gridArea={"j"}>
+        <BoxHeader
+          title="Overall Summary and Explanation Data"
+          sideText={"+18%"}
+        />
+        <Box height={15} borderRadius="1rem" bgcolor={"#cfcdef"}>
+          <Box
+            height={15}
+            width={"40%"}
+            borderRadius="1rem"
+            bgcolor={"#8884d8"}
+          ></Box>
+
+          <Typography variant="h6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            hendrerit imperdiet justo in lobortis. Proin eget ornare nisi, a
+            lacinia neque. Aliquam augue libero, elementum et porttitor sed,
+            fermentum ut metus. Pellentesque habitant morbi tristique senectus
+            et netus et malesuada fames ac turpis egestas.{" "}
+          </Typography>
+        </Box>
+      </AnalyticsBox>
     </>
   );
 };
